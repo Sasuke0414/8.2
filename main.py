6a48c1a3ac2ce9f6060bc5c8ebda7054a9f1d6bd
Abdulloh <163301172+Sasuke0414@users.noreply.github.com>
2024-09-21 09:32:25 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,44 @@
+# list
+# abdulloh = ["abdulloh", "99987654321", "Pabeda"]
+
+# print(f"{abdulloh[0]}, {abdulloh[1]}, {abdulloh[2]}da yashaydi!")
+
+# sonlar = [1, 2, 3, 4, 5, 6, 7]
+
+# sonlar.append(9)
+# sonlar.remove(4)
+# sonlar.pop()
+# sonlar.reverse()
+# sonlar.sort()
+# sonlar.insert(2, 4)
+# kattasi = max(sonlar)
+# kichigi = min(sonlar)
+
+# print(kattasi)
+
+
+
+
+
+# UY VAZIFA TEGIDA 
+
+# royhat = []
+# royhat.append(1)
+# print(royhat)
+# ------------------------------------------
+# nomlar = [1, 2, 3, 4, 5, 6]
+# nomlar.index(2)
+# print(nomlar)
+# ------------------------------------------
+# davlat = ["ozbekiston, Rossia, USA"]
+# davlat.insert(2,"Ukraina")
+# print(davlat)
+# --------------------------------------------
+# raqamlar = [2, 3, 5, 7, 1]
+# raqamlar.pop()
+# print(raqamlar)
+# ---------------------------------------------
+# masala = [1, 2, 3, 2, 4, 1, 2]
+# masala.remove(1)
+# print(masala)
+# -----------------------------------------------
